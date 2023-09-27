9eea9fad5237fe216201f6d7e4b1350ac5a877a1
MOHD NEHAL KHAN <nehalk482@gmail.com>
2023-09-27 11:26:13 +0530

    A simple C++ & if else program

---
+++ Program1.cpp
@@ -0,0 +1,8 @@
+/*SIMPLE C++ PROGRAM*/
+
+#include <iostream>
+int main()
+{
+std::cout<< "Welcome to the programming language C++ ";
+return 0;
+}
---
+++ Program2.cpp
@@ -0,0 +1,15 @@
+/*PROGRAM USING IF ELSE STATEMENT*/
+
+#include <iostream>
+int main()
+{
+int x;
+std::cout<< "enter the value of x\n";
+std::cin >> x;
+if(x%2 == 0)
+std::cout<< "Print even number";
+else
+std::cout<< "Print odd number";
+
+return 0;
+}
